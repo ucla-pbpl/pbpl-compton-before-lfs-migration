5d245c9023b04f0f36436c85eb63c51ee2b5e64e
Brian Naranjo <brian.naranjo@gmail.com>
2019-06-07 13:42:34 -0700

    Add 'gas' analysis for lil-cpt

---
+++ share/lil-cpt/analysis/gas/gas.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import Geant4 as g4
+from Geant4.hepunit import *
+import numpy as np
+
+def gamma_spray():
+    energy = 1*MeV
+    y0 = 0*mm
+    while 1:
+        yield 'gamma', g4.G4ThreeVector(0, y0,-25*mm), g4.G4ThreeVector(0,0,1), energy
---
+++ share/lil-cpt/analysis/gas/run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import toml
+from pbpl import compton
+import copy
+
+def reconf(conf, material, num_events):
+    result = copy.deepcopy(conf)
+    result['Geometry']['World']['Material'] = material
+    result['Detectors']['ComptonScintillator']['File'] = material + '.h5'
+    result['PrimaryGenerator']['NumEvents'] = num_events
+    return result
+
+def main():
+    tr = compton.TaskRunner()
+    conf = toml.load('gas.toml')
+    tr.add_task(compton.Task(reconf(conf, 'G4_Galactic', 1000000), 'vacuum'))
+    tr.add_task(compton.Task(reconf(conf, 'G4_AIR', 1000000), 'helium'))
+    tr.add_task(compton.Task(reconf(conf, 'G4_He', 1000000), 'helium'))
+    tr.add_task(compton.Task(reconf(conf, 'G4_Xe', 1000000), 'helium'))
+    tr.run()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
